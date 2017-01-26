7769d4fef90723aad596679bffdd2e8ff9f73e6b
Bachir Abi Habib <bashir.abihabib@gmail.com>
2017-01-26 14:29:55 -0600

    started on user story 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,9 @@
 (function(){
+    function config($stateProvider) {
+        $stateProvider
+            
+        
+    }
     angular
         .module(blocChat, ['ui-router', 'firebase'])
         
